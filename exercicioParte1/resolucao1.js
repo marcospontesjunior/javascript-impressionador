0fa1299cbc9e45b8514ed80c4d20d760e0dd3570
Marcos Pontes Junior <127991089+marcospontesjunior@users.noreply.github.com>
2023-05-23 20:43:50 +0100

    Update resolucao1.js

--- exercicioParte1/resolucao1.js
+++ exercicioParte1/resolucao1.js
@@ -3,15 +3,15 @@ console.log('Resultado do exercício 1');
 const primeiraVariavel = 2148;
 let segundaVariavel; 
 
-console.log(ˋO valor da primeira variável é ${primeiraVariavel}ˋ);
+console.log(`O valor da primeira variável é ${primeiraVariavel}`);
 // O valor da primeira variável é 2148
 
 segundaVariavel = 23;
 
-console.log(ˋMinha segunda variável vale ${segundaVariavel}ˋ);
+console.log(`Minha segunda variável vale ${segundaVariavel}`);
 // Minha segunda variável vale 23
 
-console.log(ˋO valor da soma das minhas duas variáveis é ${primeiraVariavel + segundaVariavel}ˋ);
+console.log(`O valor da soma das minhas duas variáveis é ${primeiraVariavel + segundaVariavel}`);
 // O valor da soma das minhas duas variáveis é 2171
 
 segundaVariavel = segundaVariavel ** 2;
@@ -25,17 +25,17 @@ console.log('Resultado do exercício 2');
 const booleana1 = true;
 const booleana2 = false;
 
-console.log(ˋOperação AND entre as duas variáveis: ${booleana1 && booleana2}ˋ); 
+console.log(`Operação AND entre as duas variáveis: ${booleana1 && booleana2}`); 
 // Operação AND entre as duas variáveis: false
 
-console.log(ˋOperação OR entre as duas variáveis: ${booleana1 || booleana2}ˋ); 
+console.log(`Operação OR entre as duas variáveis: ${booleana1 || booleana2}`); 
 // Operação AND entre as duas variáveis: true
 
 const booleana3 = false;
 
-console.log(ˋOperação AND entre as três variáveis: ${booleana1 && booleana2 && booleana3}ˋ); 
+console.log(`Operação AND entre as três variáveis: ${booleana1 && booleana2 && booleana3}`); 
 // Operação AND entre as duas variáveis: false
 
-console.log(ˋOperação OR entre as três variáveis: ${booleana1 || booleana2 || booleana3}ˋ); 
+console.log(`Operação OR entre as três variáveis: ${booleana1 || booleana2 || booleana3}`); 
 // Operação AND entre as duas variáveis: true
 
