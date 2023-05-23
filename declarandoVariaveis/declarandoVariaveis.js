cbcac59b33202217229c35c39015ac5fb759073c
Marcos Pontes Junior <127991089+marcospontesjunior@users.noreply.github.com>
2023-05-23 21:40:30 +0100

    Update declarandoVariaveis

---
+++ declarandoVariaveis/declarandoVariaveis.js
@@ -0,0 +1,18 @@
+let variavel0 = 'String'; 
+// variável que poderá ser alterada futuramente 
+
+const variavel1 = 'Meu nome é Marcos Pontes Junior e não pode ser alterado';
+// var fixa
+
+variavel2 = 'teste' 
+// var sem definição não funcionará no código
+
+// var devem ter seu nome (identificador) sem caracteres especiais,
+// excerto $ e _   
+
+// var não podem ter seus identificadores começando com número,
+// elas podem, porém, ter números ao longo de seu nome.
+
+// também não podem usar palavras reservadas do JavaScript como nome de var.
+
+// respeitar o padrão camelCase para a nomeclatura de var.
