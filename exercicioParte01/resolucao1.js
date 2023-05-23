2132fb6d6dc87d66da694843f4410bfd74620111
Marcos Pontes Junior <127991089+marcospontesjunior@users.noreply.github.com>
2023-05-23 18:26:37 +0100

    Update exercicioParte01

---
+++ exercicioParte01/resolucao1.js
@@ -0,0 +1,41 @@
+console.log('Resultado do exercício 1');
+
+const primeiraVariavel = 2148;
+let segundaVariavel; 
+
+console.log(ˋO valor da primeira variável é ${primeiraVariavel}ˋ);
+// O valor da primeira variável é 2148
+
+segundaVariavel = 23;
+
+console.log(ˋMinha segunda variável vale ${segundaVariavel}ˋ);
+// Minha segunda variável vale 23
+
+console.log(ˋO valor da soma das minhas duas variáveis é ${primeiraVariavel + segundaVariavel}ˋ);
+// O valor da soma das minhas duas variáveis é 2171
+
+segundaVariavel = segundaVariavel ** 2;
+console.log(segundaVariavel); // 529
+
+segundaVariavel = segundaVariavel / 3;
+console.log(segundaVariavel); // 176.333333333333334
+
+console.log('Resultado do exercício 2');
+
+const booleana1 = true;
+const booleana2 = false;
+
+console.log(ˋOperação AND entre as duas variáveis: ${booleana1 && booleana2}ˋ); 
+// Operação AND entre as duas variáveis: false
+
+console.log(ˋOperação OR entre as duas variáveis: ${booleana1 || booleana2}ˋ); 
+// Operação AND entre as duas variáveis: true
+
+const booleana3 = false;
+
+console.log(ˋOperação AND entre as três variáveis: ${booleana1 && booleana2 && booleana3}ˋ); 
+// Operação AND entre as duas variáveis: false
+
+console.log(ˋOperação OR entre as três variáveis: ${booleana1 || booleana2 || booleana3}ˋ); 
+// Operação AND entre as duas variáveis: true
+
