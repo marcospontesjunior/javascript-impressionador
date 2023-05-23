303dcb03b5823c6f751e5f9876040b3e299b5fdb
Marcos Pontes Junior <127991089+marcospontesjunior@users.noreply.github.com>
2023-05-23 21:52:37 +0100

    Update Modulo2

---
+++ operadoresAritmeticos/operadoresAritmeticos.js
@@ -0,0 +1,25 @@
+const resultado0 = 1 + 2; // Soma
+const resultado1 = 2 - 2; // Subtração
+const resultado2 = 4 / 2; // Divisão
+const resultado3 = 4 * 2; // Multiplicação
+const resultado4 = 2 ** 3; // Potenciação
+const resultado5 = 29 % 7; // Resto da Divisão (mod)
+
+let resultado6 = 1; // Incrementar 
+resultado6++;
+
+let resultado7 = 1; // Decrementar 
+resultado7--;
+
+const texto0 = 'Operadores também ';
+const texto1 = 'funcionam com outros tipos de dados!';
+
+console.log(resultado0) // 3
+console.log(resultado1) // 0
+console.log(resultado2) // 2
+console.log(resultado3) // 8
+console.log(resultado4) // 8
+console.log(resultado5) // 1
+console.log(resultado6) // 2
+console.log(resultado7) // 1
+console.log(text0 + text1) // Operadores também funcionam com outros tipos ded dados!
---
+++ tiposDados/tiposDados.js
@@ -0,0 +1,18 @@
+const text0 = 'String com Aspas Simples';
+const text1 = "String com Aspas Duplas";
+const text2 = `String com Crase`;
+
+const registrarNumero = 22543;
+
+const stringTemplate = `Registrando o número registrado é ${registrarNumero}`; 
+// "formatação" especial usar (`) 
+
+console.log(stringTemplate);
+
+const numero0 = 27; // variável int
+const numero1 = 23.25; // variável float
+const numero2 = -40; // variável num Negativa
+
+const total = numero0 + numero1 + numero2;
+
+console.log(total); // total = 10.25
