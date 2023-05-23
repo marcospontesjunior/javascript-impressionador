28e5b22ab186ea0746adfa4cb15e2afea9df51f8
Marcos Pontes Junior <127991089+marcospontesjunior@users.noreply.github.com>
2023-05-23 22:03:46 +0100

    Update modulo2

--- modulo2/operadoresAritmeticos/operadoresAritmeticos.js
+++ modulo2/operadoresAritmeticos/operadoresAritmeticos.js
@@ -1,14 +1,14 @@
-const resultado0 = 1 + 2; // Soma
-const resultado1 = 2 - 2; // Subtração
-const resultado2 = 4 / 2; // Divisão
-const resultado3 = 4 * 2; // Multiplicação
-const resultado4 = 2 ** 3; // Potenciação
-const resultado5 = 29 % 7; // Resto da Divisão (mod)
+const resultado0 = 1 + 2; // soma
+const resultado1 = 2 - 2; // subtração
+const resultado2 = 4 / 2; // divisão
+const resultado3 = 4 * 2; // multiplicação
+const resultado4 = 2 ** 3; // potenciação
+const resultado5 = 29 % 7; // resto da divisão
 
-let resultado6 = 1; // Incrementar 
+let resultado6 = 1; // incrementar 
 resultado6++;
 
-let resultado7 = 1; // Decrementar 
+let resultado7 = 1; // decrementar 
 resultado7--;
 
 const texto0 = 'Operadores também ';
@@ -22,4 +22,4 @@ console.log(resultado4) // 8
 console.log(resultado5) // 1
 console.log(resultado6) // 2
 console.log(resultado7) // 1
-console.log(text0 + text1) // Operadores também funcionam com outros tipos ded dados!
+console.log(text0 + text1) // operadores também funcionam com outros tipos ded dados!
